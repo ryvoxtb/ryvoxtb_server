f6eb930f042d90466ecc2d652a93757bdf6f5f10
REVOX Tv <ryvox.tb@gmail.com>
2025-10-27 04:25:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // ✅ Blogger-only access
 app.use((req, res, next) => {
-  const allowedOrigin = "https://mdabdullahsk.blogspot.com";
+  const allowedOrigin = "https://ryvoxtb.github.io/ryvoxtb/index.html";
   const origin = req.headers.origin || req.headers.referer || "";
 
   if (origin && origin.startsWith(allowedOrigin)) {
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 // ✅ CORS setup
 app.use(cors({
-  origin: "https://mdabdullahsk.blogspot.com",
+  origin: "https://ryvoxtb.github.io/ryvoxtb/index.html",
   methods: ["GET"],
 }));
 
